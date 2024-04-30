a66811f0b1513b8a15635f50c4f9c971fe4041ae
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-30 12:20:37 +0200

    Adding Images in NextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+// import homeImg from '../../public/home.jpg';
+import homeImg from 'public/home.jpg';
+
 export default function Home() {
   return (
     <div>
       Home Page
+      <div className="absolute -z->0 inset-0">
+        <Image src={homeImg} alt="car factory" fill style={{ objectFit: 'cover' }} />
+      </div>
     </div>
   );
 }
